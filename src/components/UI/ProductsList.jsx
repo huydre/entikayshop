63b10643fcea0da40946b9422ac26f0e43fda5ea
HNamm <80162506+huydre@users.noreply.github.com>
2023-04-04 11:41:05 +0700

    remove motion in List Product

--- src/components/UI/ProductsList.jsx
+++ src/components/UI/ProductsList.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import ProductsCard from './ProductsCard';
-import { motion } from 'framer-motion';
 
 const ProductsList = ({data}) => {
     return (
-        <motion.div
-            initial={{opacity: 0, y: 50}}
-            whileInView={{opacity: 1, y: 0, transition: {
-                type: 'spring',
-                duration: 1.25,
-                elay: 1.1
-            }}}
-            viewport={{ once: true, amount: 0.25}}
-        >
+        <>
         {data.map((item, index) => (
             <ProductsCard item={item} key={index}/>
         ))}
-        </motion.div>
+        </>
     )
 }
 
